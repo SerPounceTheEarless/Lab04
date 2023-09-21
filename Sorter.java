cad4b9398f63266c670a8477d7a1445e966f9a6a
serpouncetheearless <abdulrahmanalzamil9@gmail.com>
2023-09-21 16:18:16 +0300

    Selection Sort added

--- Sorter.java
+++ Sorter.java
@@ -1,5 +1,47 @@
-/*X5VI is the goat*/
-يخسي الهطف
+import java.util.Scanner;
 
+public class Sorter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
 
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        selectionSort(arr);
+
+        System.out.println("Sorted array:");
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+
+        scanner.close();
+    }
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
