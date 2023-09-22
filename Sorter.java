40fe36fe2e4c5330273c09974b4447e7758c9aea
AbdulmalikAk47 <abdulmalikwaleed5@gmail.com>
2023-09-22 17:38:25 +0300

    Adding Insertion Sort method

--- Sorter.java
+++ Sorter.java
@@ -22,6 +22,12 @@ public class Sorter {
             System.out.print(value + " ");
         }
 
+        int array[] = new int[7];
+
+        for(int i = 0; i < 7; i++) {
+            array[i] = i - 2;
+        }
+
         scanner.close();
     }
 
@@ -42,6 +48,20 @@ public class Sorter {
             int temp = arr[minIndex];
             arr[minIndex] = arr[i];
             arr[i] = temp;
+        }
+    }
+    public static void sort(int arr[]) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+ 
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
         }
     }
 }
